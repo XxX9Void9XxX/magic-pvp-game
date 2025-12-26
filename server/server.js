25c52fcd3edb3ab589c4047a30c858402265cb65
XxX9Void9XxX <brooksm@carbonschools.org>
2025-12-25 18:11:03 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,23 +8,25 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "../client")));
-app.get("*", (req,res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
 const players = {};
 
 io.on("connection", socket => {
+
   players[socket.id] = {
     x: 100, y: 100, id: socket.id,
     health: 100
   };
+
   io.emit("players", players);
 
-  socket.on("move", d => {
+  socket.on("move", data => {
     if (players[socket.id]) {
-      players[socket.id].x = d.x;
-      players[socket.id].y = d.y;
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
       io.emit("players", players);
     }
   });
@@ -37,8 +39,17 @@ io.on("connection", socket => {
     if (players[id]) {
       players[id].health -= 10;
       if (players[id].health <= 0) {
-        io.emit("playerDied", id);
         delete players[id];
+
+        setTimeout(() => {
+          players[id] = {
+            x: Math.random() * 2000 + 100,
+            y: Math.random() * 2000 + 100,
+            id: id,
+            health: 100
+          };
+          io.emit("respawnPlayer", players[id]);
+        }, 2000);
       }
       io.emit("players", players);
     }
